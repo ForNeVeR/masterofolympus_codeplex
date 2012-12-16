8fddded1cce6348d521621b28383e8f8c4bdb645
Adrien GARIN <moguchiy.cs@gmail.com>
2012-12-16 14:47:32 +0100

    Optimisation du rendu.

---
+++ Audio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace Master_Of_Olympus
+{
+    public class Audio
+    {
+        private Song m_current_song;
+
+        public Audio()
+        {
+        }
+
+        public void LoadAudioContent(ContentManager content_manager)
+        {
+            m_current_song = content_manager.Load<Song>("Music/Amolfi");
+            //MediaPlayer.Play(m_current_song);
+        }
+    }
+}
---
+++ Resources.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Master_Of_Olympus
+{
+    public static class Resources
+    {
+        public static Dictionary<int, Texture2D> terrain_textures = new Dictionary<int, Texture2D>();
+        public static Dictionary<int, Texture2D> hydra_anim_right_textures = new Dictionary<int, Texture2D>();
+
+        public static void LoadTerrainTextures(ContentManager content_manager)
+        {
+            for (int i = 1; i <= 163; ++i)
+            {
+                string str = "Zeus_land1_" + i.ToString("00000");
+                terrain_textures.Add(i, content_manager.Load<Texture2D>("Zeus_Terrain/" + str));
+            }
+        }
+
+        public static void LoadHydraTextures(ContentManager content_manager)
+        {
+            for (int i = 2; i <= 122; i += 8)
+            {
+                string str = "zeus_hydra_" + i.ToString("00000");
+                hydra_anim_right_textures.Add(i, content_manager.Load<Texture2D>("Zeus_Hydra/anim_right/" + str));
+            }
+        }
+    }
+}
